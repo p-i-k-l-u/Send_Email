ec7b6a83bf0dbda4307b376f7b61a9e8a6a5e1d8
p-i-k-l-u <piklu19jan@gmail.com>
2026-03-12 13:02:53 +0530

    something change

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,49 +1,9 @@
-//package listeners;
-//
-//import org.testng.*;
-//
-//import utils.EmailSender;
-//import utils.HtmlTemplateBuilder;
-//
-//public class TestListener implements ITestListener {
-//
-//	int passed = 0;
-//	int failed = 0;
-//	int skipped = 0;
-//
-//	public void onTestSuccess(ITestResult result) {
-//		passed++;
-//	}
-//
-//	public void onTestFailure(ITestResult result) {
-//		failed++;
-//	}
-//
-//	public void onTestSkipped(ITestResult result) {
-//		skipped++;
-//	}
-//
-//	public void onFinish(ITestContext context) {
-//
-//		int executed = passed + failed + skipped;
-//
-//		int planned = context.getAllTestMethods().length;
-//
-//		String html = HtmlTemplateBuilder.buildHtml(planned, executed, passed, failed, skipped);
-//
-//		EmailSender.sendEmail(html);
-//
-//	}
-//
-//}
-
-// -------------------------------------- AWS ------------------------------------------------
-
 package listeners;
 
 import org.testng.*;
-import utils.HtmlTemplateBuilder;
+
 import utils.EmailSender;
+import utils.HtmlTemplateBuilder;
 
 public class TestListener implements ITestListener {
 
@@ -66,12 +26,52 @@ public class TestListener implements ITestListener {
 	public void onFinish(ITestContext context) {
 
 		int executed = passed + failed + skipped;
+
 		int planned = context.getAllTestMethods().length;
 
 		String html = HtmlTemplateBuilder.buildHtml(planned, executed, passed, failed, skipped);
 
-		EmailSender.sendEmail("Automation Test Report", html);
+		EmailSender.sendEmail(html);
 
 	}
 
 }
+
+// -------------------------------------- AWS ------------------------------------------------
+
+//package listeners;
+//
+//import org.testng.*;
+//import utils.HtmlTemplateBuilder;
+//import utils.EmailSender;
+//
+//public class TestListener implements ITestListener {
+//
+//	int passed = 0;
+//	int failed = 0;
+//	int skipped = 0;
+//
+//	public void onTestSuccess(ITestResult result) {
+//		passed++;
+//	}
+//
+//	public void onTestFailure(ITestResult result) {
+//		failed++;
+//	}
+//
+//	public void onTestSkipped(ITestResult result) {
+//		skipped++;
+//	}
+//
+//	public void onFinish(ITestContext context) {
+//
+//		int executed = passed + failed + skipped;
+//		int planned = context.getAllTestMethods().length;
+//
+//		String html = HtmlTemplateBuilder.buildHtml(planned, executed, passed, failed, skipped);
+//
+//		EmailSender.sendEmail("Automation Test Report", html);
+//
+//	}
+//
+//}
--- src/test/java/utils/EmailSender.java
+++ src/test/java/utils/EmailSender.java
@@ -1,100 +1,100 @@
-//package utils;
-//
-//import java.util.Properties;
-//
-//import jakarta.mail.Authenticator;
-//import jakarta.mail.Message;
-//import jakarta.mail.PasswordAuthentication;
-//import jakarta.mail.Session;
-//import jakarta.mail.Transport;
-//import jakarta.mail.internet.InternetAddress;
-//import jakarta.mail.internet.MimeMessage;
-//
-//public class EmailSender {
-//
-//    public static void sendEmail(String html) {
-//
-//        String from = "piklu@genefied.ai";           // your Gmail
-//        String password = "khyo dqai qjnj bmjq";       // Gmail App Password
-//        String to = "piklude34@gmail.com";             // recipient
-//
-//        Properties props = new Properties();
-//
-//        props.put("mail.smtp.host", "smtp.gmail.com");
-//        props.put("mail.smtp.port", "587");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//
-//        Session session = Session.getInstance(props,
-//            new Authenticator() {
-//                protected PasswordAuthentication getPasswordAuthentication() {
-//                    return new PasswordAuthentication(from, password);
-//                }
-//            });
-//
-//        try {
-//
-//            Message message = new MimeMessage(session);
-//
-//            message.setFrom(new InternetAddress(from));
-//
-//            message.setRecipients(
-//                    Message.RecipientType.TO,
-//                    InternetAddress.parse(to));
-//
-//            message.setSubject("Selenium Automation Report");
-//
-//            // HTML content
-//            message.setContent(html, "text/html");
-//
-//            // DEBUG: Print before sending
-//            System.out.println("Sending Email...");
-//
-//            Transport.send(message);
-//
-//            // DEBUG: Print after sending
-//            System.out.println("Email Sent Successfully");
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
-
-// -----------------------------------------------  AWS Related --------------------------------------
-
 package utils;
 
-import software.amazon.awssdk.services.ses.SesClient;
-import software.amazon.awssdk.services.ses.model.*;
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 public class EmailSender {
 
-	public static void sendEmail(String subject, String html) {
+    public static void sendEmail(String html) {
 
-		String from = "piklu@genefied.ai";
-		String to = "piklude34@gmail.com";
+        String from = "piklu@genefied.ai";           // your Gmail
+        String password = "khyo dqai qjnj bmjq";       // Gmail App Password
+        String to = "piklude34@gmail.com";             // recipient
 
-		SesClient sesClient = SesClient.create();
+        Properties props = new Properties();
 
-		Destination destination = Destination.builder().toAddresses(to).build();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
 
-		Content subjectContent = Content.builder().data(subject).build();
+        Session session = Session.getInstance(props,
+            new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(from, password);
+                }
+            });
 
-		Content htmlContent = Content.builder().data(html).build();
+        try {
 
-		Body body = Body.builder().html(htmlContent).build();
+            Message message = new MimeMessage(session);
 
-		Message message = Message.builder().subject(subjectContent).body(body).build();
+            message.setFrom(new InternetAddress(from));
 
-		SendEmailRequest request = SendEmailRequest.builder().source(from).destination(destination).message(message)
-				.build();
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(to));
 
-		sesClient.sendEmail(request);
+            message.setSubject("Selenium Automation Report");
 
-		System.out.println("Email Sent Successfully");
+            // HTML content
+            message.setContent(html, "text/html");
 
-	}
+            // DEBUG: Print before sending
+            System.out.println("Sending Email...");
 
+            Transport.send(message);
+
+            // DEBUG: Print after sending
+            System.out.println("Email Sent Successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
+
+// -----------------------------------------------  AWS Related --------------------------------------
+
+//package utils;
+//
+//import software.amazon.awssdk.services.ses.SesClient;
+//import software.amazon.awssdk.services.ses.model.*;
+//
+//public class EmailSender {
+//
+//	public static void sendEmail(String subject, String html) {
+//
+//		String from = "piklu@genefied.ai";
+//		String to = "piklude34@gmail.com";
+//
+//		SesClient sesClient = SesClient.create();
+//
+//		Destination destination = Destination.builder().toAddresses(to).build();
+//
+//		Content subjectContent = Content.builder().data(subject).build();
+//
+//		Content htmlContent = Content.builder().data(html).build();
+//
+//		Body body = Body.builder().html(htmlContent).build();
+//
+//		Message message = Message.builder().subject(subjectContent).body(body).build();
+//
+//		SendEmailRequest request = SendEmailRequest.builder().source(from).destination(destination).message(message)
+//				.build();
+//
+//		sesClient.sendEmail(request);
+//
+//		System.out.println("Email Sent Successfully");
+//
+//	}
+//
+//}
